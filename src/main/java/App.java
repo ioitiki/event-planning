dd225d2930e9d359b7713977dc218b4d409e6e6b
Andrew Malkin <andymalkin@hotmail.com>
2017-03-17 10:41:44 -0700

    add basic console front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,12 +5,43 @@ import static java.util.Arrays.asList;
 
 public class App {
   public static void main(String[] args){
-    Console myConsole = System.console();
+    Console console = System.console();
 
     String couponCode1 = "coupon1";
     String couponCode2 = "coupon2";
-
     
+    System.out.println("-----------------------------------------------------");
+    System.out.println("How many guests are you expecting?");
+    int userNumberOfGuests = Integer.parseInt(console.readLine());
+
+    System.out.println("-----------------------------------------------------");
+    System.out.println("What type of food would you like to serve at your event? Choose from the following: Indian, Veggie, Burgers, or Thai.");
+    String userTypeOfFood = console.readLine();
+
+    System.out.println("-----------------------------------------------------");
+    System.out.println("What type of drinks would you like to serve at your event? Choose from the following: Beer, Wine, or Juice.");
+    String userTypeOfDrink = console.readLine();
+
+    System.out.println("-----------------------------------------------------");
+    System.out.println("What type of entertainment would you like to provide at yor event? Choose from the following: Live Band, DJ, Petting Zoo, or None");
+    String userTypeOfEntertainment = console.readLine();
+
+    Event userEvent = new Event(userNumberOfGuests, userTypeOfFood, userTypeOfDrink, userTypeOfEntertainment);
+
+    int eventCost = Event.calculateEventCost(userEvent.getNumberOfGuests(), userEvent.getTypeOfFood(), userEvent.getTypeOfDrink(), userEvent.getTypeOfEntertainment());
+
+    System.out.println("-----------------------------------------------------");
+    System.out.println("\nYour total cost is $" + eventCost + ".00.\n");
+
+    // System.out.println("__________________________________________________");
+    // System.out.println("If you have a coupon code please enter it now to see your
+    //                     discounted price.");
+    // String userCoupon = console.readLine();
+    //
+    // if (userCoupon.equals("couponCode1")) {
+    //   int eventCostWithCoupon1 = eventCost - user
+    //   System.out.println()
+    // }
 
   }
 }
